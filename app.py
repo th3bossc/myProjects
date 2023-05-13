c2240bac5f92937794f7fb2d6965ba35e3c19376
th3bossc <diljith2003@gmail.com>
2023-05-14 01:14:22 +0530

    bug fixes

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from threading import Thread
 
 app = create_app()
 
-# if __name__ == '__main__':
-#     app.run(debug = True)
+if __name__ == '__main__':
+    app.run(debug = True)
 
 def run():
     app.run(host = '0.0.0.0', port = 8000)
--- myProjects/main/routes.py
+++ myProjects/main/routes.py
@@ -6,7 +6,7 @@ main = Blueprint('main', __name__)
 @main.route('/')
 @main.route('/home')
 def home():
-    page = request.args.get('page', default = 1)
+    page = request.args.get('page', type = int, default = 1)
     posts = Post.query.order_by(Post.date_posted.desc()).paginate(page = page, per_page = 5)
     return render_template('home.html', title = 'Home', page_details = "My Projects", posts = posts, page = page)
 
